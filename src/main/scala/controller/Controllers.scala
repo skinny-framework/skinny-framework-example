666c11eb442f17952acf9f411834663212add17c
xuwei-k <6b656e6a69@gmail.com>
2019-08-10 10:56:07 +0900

    avoid deprecated scala.Symbol literals

--- src/main/scala/controller/Controllers.scala
+++ src/main/scala/controller/Controllers.scala
@@ -13,7 +13,7 @@ object Controllers {
   }
 
   object root extends RootController with Routes {
-    val indexUrl = get("/?")(index).as('index)
+    val indexUrl = get("/?")(index).as(Symbol("index"))
   }
 
   object members extends _root_.controller.MembersController {
@@ -24,33 +24,33 @@ object Controllers {
     // --------------
     // show
 
-    val indexUrl = get("/companies")(showResources).as('index)
+    val indexUrl = get("/companies")(showResources).as(Symbol("index"))
     val showUrl = get("/companies/:id") {
       params.getAs[Long]("id").map(id => showResource(id)).getOrElse(haltWithBody(404))
-    }.as('show)
+    }.as(Symbol("show"))
 
     // --------------
     // create
 
-    val newUrl = get("/companies/new")(newResource).as('new)
-    val createUrl = post("/companies")(createResource).as('create)
+    val newUrl = get("/companies/new")(newResource).as(Symbol("new"))
+    val createUrl = post("/companies")(createResource).as(Symbol("create"))
 
     // --------------
     // update
 
     val editUrl = get("/companies/:id/edit") {
       params.getAs[Long]("id").map(id => editResource(id)).getOrElse(haltWithBody(404))
-    }.as('edit)
+    }.as(Symbol("edit"))
     val updateUrl = post("/companies/:id") {
       params.getAs[Long]("id").map(id => updateResource(id)).getOrElse(haltWithBody(404))
-    }.as('update)
+    }.as(Symbol("update"))
 
     // --------------
     // delete
 
     val destroyUrl = delete("/companies/:id") {
       params.getAs[Long]("id").map(id => destroyResource(id)).getOrElse(haltWithBody(404))
-    }.as('destroy)
+    }.as(Symbol("destroy"))
 
   }
 
